b6ffbffe9b5bf9e2158e1726dd6a017800bf1f9f
Shirley Keter <shirleyketer@gmail.com>
2019-04-23 09:26:42 +0300

    create function for update request

---
+++ app/main/views.py
@@ -0,0 +1,35 @@
+from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+    
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form
