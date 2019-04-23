a98c0f01c952088a59855c2794f44c03673fae6e
Shirley Keter <shirleyketer@gmail.com>
2019-04-23 10:43:06 +0300

    initialize mail class

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -20,6 +21,5 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    db.init_app(app)
     login_manager.init_app(app)
-    
+    mail.init_app(app)
