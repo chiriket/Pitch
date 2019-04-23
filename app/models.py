95d2fde38d5c63e172307323caa4dc2bba3dbcb8
Shirley Keter <shirleyketer@gmail.com>
2019-04-23 09:18:54 +0300

    added user profile

--- app/models.py
+++ app/models.py
@@ -1,13 +1,16 @@
 from . import db
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.username}'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
 class Role(db.Model):
     __tablename__ = 'roles'
