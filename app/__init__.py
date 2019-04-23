57bdf409734836b9fde18535de22fee1de2050dd
Shirley Keter <shirleyketer@gmail.com>
2019-04-23 09:31:04 +0300

    added update configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +14,12 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+     # configure UploadSet
+    configure_uploads(app,photos)
+
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class   
     '''
-    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://chiri:1234@localhost/watchlist'
 
